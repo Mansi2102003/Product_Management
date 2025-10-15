c5c33d7a6c38056b48a854bedea592acffbafe6f
Mansi Thakkar <mthakkar@logilite.com>
2025-10-15 11:17:32 +0530

    "Updated DB Connection..Removed indirect connection and added direct path to connect with database, it is simpler and easy to understand"

--- src/main/java/com/demo/product/util/DBConnection.java
+++ src/main/java/com/demo/product/util/DBConnection.java
@@ -3,31 +3,17 @@ package com.demo.product.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
-import java.io.InputStream;
 
 
 //Handles PostgreSQL DB connection using properties file.
+
 public class DBConnection {
-
-    private static String url;
-    private static String user;
-    private static String password;
-
-    static {
-        try (InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("application.properties")) {
-            Properties props = new Properties();
-            props.load(input);
-            url = props.getProperty("db.url");
-            user = props.getProperty("db.user");
-            password = props.getProperty("db.password");
-            Class.forName("org.postgresql.Driver");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    private static final String URL = "jdbc:postgresql://localhost:5432/productdb";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "mansi0210";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(url, user, password);
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
