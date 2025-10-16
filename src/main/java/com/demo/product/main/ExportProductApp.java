7c0c8fa37853bf43cefbdcc932f4dbadffec5729
Mansi Thakkar <mthakkar@logilite.com>
2025-10-16 16:55:56 +0530

    "Implemented ExportProductApp class which is entry point of execution" ExportProductApp class calls the service class method to retrive Products from database and Write into Excel to database.

---
+++ src/main/java/com/demo/product/main/ExportProductApp.java
@@ -0,0 +1,12 @@
+package com.demo.product.main;
+
+import com.demo.product.service.ProductExportService;
+
+//Entry point for Execution of program to Export data from database
+public class ExportProductApp {
+	public static void main(String[] args) {
+		
+		//Calls Service class method which contains the logic of export products from database
+		new ProductExportService().exportProducts();
+	}
+}
